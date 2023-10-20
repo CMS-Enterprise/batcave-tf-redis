ad34562a8bf2b7d9c01700640e348ebffb4e1be1
Hasan Tutac <hasantutac@ip-10-232-53-245.ec2.internal>
2023-10-20 11:56:35 -0400

    Added variables for integration

--- main.tf
+++ main.tf
@@ -1,85 +1,28 @@
+
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # Set your desired AWS region
-}
-
-# vpc id
-resource "aws_subnet" "example" {
-  vpc_id     = "vpc-0f45adf48c2422e8a"
-  cidr_block = "10.202.196.0/23"
-
-  tags = {
-    Name = "my-subnet"
-  }
-}
-
-# private subnets
-data "aws_subnets" "private" {
-  filter {
-    name = "tag:Name"
-    values = [
-      try(var.subnet_lookup_overrides.private, "${var.project}-*-${var.env}-private-*")
-    ]
-  }
-}
-
-# public subnets
-data "aws_subnets" "public" {
-  filter {
-    name = "tag:Name"
-    values = [
-      try(var.subnet_lookup_overrides.public, "${var.project}-*-${var.env}-public-*")
-    ]
-  }
+  region = "us-east-1"
 }
 
 
-
-## subnet resources
-data "aws_subnet" "private" {
-  for_each = toset(data.aws_subnets.private.ids)
-  id       = each.value
-}
 
 
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "redis-eppe"
-  engine               = "redis"
-  engine_version       = "6.x"
-  node_type            = "cache.t4.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
-  port                 = 6379
-  security_group_ids   = [aws_security_group.redis.id]
+  cluster_id           = var.cluster_id 
+  engine               = var.engine
+  engine_version       = var.engine_version
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  parameter_group_name = var.parameter_group_name
+  port                 = var.port
+  security_group_ids   = var.security_group_ids
 
-  subnet_group_name = aws_elasticache_subnet_group.redis.name
+  subnet_group_name = var.subnet_group_name
 
   tags = var.tags
 }
 
-resource "aws_security_group" "redis" {
-  name_prefix = "${var.project}-redis-${var.env}"
-  vpc_id      = "vpc-0f45adf48c2422e8a"
-
-  // egress {
-  //   from_port        = 0
-  //   to_port          = 0
-  //   protocol         = "-1"
-  //   cidr_blocks      = ["0.0.0.0/0"]
-  //   ipv6_cidr_blocks = ["::/0"]
-  // }
-
-  // tags = {
-  //   Name = "allow_tls"
-  // }
-}
 
 
-resource "aws_elasticache_subnet_group" "redis" {
-  name       = "${var.project}-redis-${var.env}"
-  description = "My subnet group"
-  subnet_ids = [aws_subnet.example.id] # Replace with your subnet IDs
-}
 
-// output "redis_endpoint" {
-//   value = aws_elasticache_cluster.redis.cache_nodes.0.endpoint
-// }
+
--- variables.tf
+++ variables.tf
@@ -11,34 +11,50 @@ variable "aws_region" {
 }
 
 
-variable "vpc_lookup_override" {
-  description = "Some VPCs don't follow standard naming conventions.  Use this to override the query used to lookup VPC names.  Accepts wildcard in form of '*'"
+variable "cluster_id" {
   default     = ""
   type        = string
 }
 
-variable "subnet_lookup_overrides" {
-  description = "Some Subnets don't follow standard naming conventions.  Use this map to override the query used for looking up Subnets.  Ex: { private = \"foo-west-nonpublic-*\" }"
-  default     = {}
-  type        = map(string)
-}
-
-variable "create_s3_vpc_endpoint" {
-  type        = bool
-  description = "toggle on/off the creation of s3 vpc endpoint"
-  default     = true
-}
-
-variable "vpc_endpoint_lookup_overrides" {
-  description = "Some vpc endpoints don't follow standard naming conventions.  Use this map to override the query used for looking up Subnets.  Ex: { private = \"foo-west-nonpublic-*\" }"
+variable "engine" {
   default     = ""
   type        = string
 }
 
-variable "nat_gateways_lookup_overrides" {
-  description = "Some nat gateways don't follow standard naming conventions.  Use this map to override the query used for looking up Subnets.  Ex: { private = \"foo-west-nonpublic-*\" }"
+variable "engine_version" {
   default     = ""
   type        = string
+}
+
+variable "node_type" {
+  default     = ""
+  type        = string
+}
+
+variable "num_cache_nodes" {
+  default     = 1
+  type        = number
+}
+
+variable "parameter_group_name" {
+  default     = ""
+  type        = string
+}
+
+variable "port" {
+  default     = 6379
+  type        = number
+}
+
+
+variable "subnet_group_name" {
+  default     = ""
+  type        = string
+}
+
+variable "subnet_group_name" {
+  type    = list(string)
+  default = []
 }
 
 variable "tags" {
